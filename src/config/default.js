eecf2e62d40c71479a3306cc8fe921b23df13823
devsdocs <73208025+devsdocs@users.noreply.github.com>
2021-02-04 00:47:44 +0900

    Update default.js

--- src/config/default.js
+++ src/config/default.js
@@ -5,7 +5,7 @@ const config = {
    * to get following params: client_id, client_secret, refresh_token & redirect_uri.
    */
   refresh_token: REFRESH_TOKEN,
-  client_id: '6600e358-9328-4050-af82-0af9cdde796b',
+  client_id: 'cc435f05-b40d-41b6-a03f-20854c901e63',
   client_secret: CLIENT_SECRET,
   redirect_uri: 'https://heymind.github.io/tools/microsoft-graph-api-auth',
 
